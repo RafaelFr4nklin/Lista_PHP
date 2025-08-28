dca3a5608078f275112d395534046becb77e7c88
RafaelFr4nklin <rafael_f_de-farias@estudante.sesisenai.org.br>
2025-08-27 22:02:28 -0300

    exercicio02Finalizado

---
+++ exercicio02.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 02 tabuada</title>
+</head>
+<body>
+ <form method= "POST" action="">
+<!--POST envia dados-->
+<!-- GET procura dados -->
+ <label for="numero"> Digite um número:</label>
+    <input type="numero" id="numero" name="numero" required>
+    <button type="submit" name="verificar_tabuada">Verificar</button>
+ </form>   
+ <?php
+ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if(isset($_POST['verificar_tabuada'])) {
+    $numero = $_POST['numero'];
+    //$i é o multiplicador, não precisa ser informado antes nesse caso
+    for($i = 1; $i <= 10; $i++) {
+        $resultado = $numero * $i;
+        echo "$numero x $i = $resultado <br>";
+    }
+}
+ }
+ ?>
+</body>
+</html>
